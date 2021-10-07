1e666c8539dc4544c10f918fd020229ecc96b3fc
Aidarskiy <kazymbaevaidar@gmail.com>
2021-10-07 16:46:08 +0600

    first

---
+++ js.js
@@ -0,0 +1,29 @@
+// let list = [
+//     'Milk',
+//     'Sugar',
+//     'Salt',
+//     'Butter',
+//     'Bread'
+// ]
+// console.log(list[0]);
+// console.log(list[1]);
+// console.log(list[2]);
+
+// let [first, second, ...other] = list;
+// console.log(first, second, ...other);
+
+// let arr = [
+//     [1, 2, 3, 4, [9, 0]],
+//     [5, 6, 7, 8]
+// ];
+
+// let [[a, b, c, d, [nine, zero]], [newArr, ...other]] = arr;
+// console.log(a, b, c, d, newArr, other);
+// console.log(nine, zero);
+
+let person = {
+    age: 30,
+    name: 'John',
+    lastName: 'Snow'
+};
+
